50c80040458416846eea2b5daea04277f58b84c3
rkasale28 <rohitkasale28@gmail.com>
2020-03-16 13:51:55 +0530

    Done with Customer Home Page

--- nursery24/nursery24/settings.py
+++ nursery24/nursery24/settings.py
@@ -56,7 +56,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR,'templates/adminpage'),
         os.path.join(BASE_DIR,'templates/consumer'),
-        os.path.join(BASE_DIR,'templates/provider')],
+        os.path.join(BASE_DIR,'templates/provider'),
+        os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
