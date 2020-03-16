ef31e9687790e4d43eea800e832359be1e2068ca
rkasale28 <rohitkasale28@gmail.com>
2020-03-16 19:23:49 +0530

    Home, plants, seeds, soil and fertilizers, decor, accessories for consumer

--- nursery24/consumer/urls.py
+++ nursery24/consumer/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.home,name="home"),
+    path('home',views.home,name="home"),
+    path('plants',views.plants,name="plants"),
+    path('seeds',views.seeds,name="seeds"),
+    path('soilandfertilizers',views.soil,name="soil"),
+    path('decor',views.decor,name="decor"),
+    path('accessories',views.accessories,name="accessories")
 ]
--- nursery24/consumer/views.py
+++ nursery24/consumer/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'chome.html')
+
+def plants(request):
+    return render(request,'cplants.html')
+
+def seeds(request):
+    return render(request,'cseeds.html')
+    
+def soil(request):
+    return render(request,'csoil.html')
+    
+def decor(request):
+    return render(request,'cdecor.html')
+    
+def accessories(request):
+    return render(request,'caccessories.html')
