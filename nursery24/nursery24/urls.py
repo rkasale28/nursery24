daeb48920d9fc73d68258a63a3be11e41540e944
rkasale28 <rohitkasale28@gmail.com>
2020-03-19 18:11:37 +0530

    Added Courier App

--- nursery24/nursery24/urls.py
+++ nursery24/nursery24/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('consumer/',include('consumer.urls')),
     path('provider/',include('provider.urls')),
+    path('courier/',include('courier.urls')),
     path('adminpage/',include('adminpage.urls'))
 ]
 
