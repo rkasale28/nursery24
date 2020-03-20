127e06c6fadfd08045341e83edb5d0c473f6f860
rkasale28 <rohitkasale28@gmail.com>
2020-03-20 11:26:44 +0530

    Added urls for media

--- nursery24/nursery24/urls.py
+++ nursery24/nursery24/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +25,5 @@ urlpatterns = [
     path('provider/',include('provider.urls')),
     path('administrator/',include('administrator.urls')),
     path('courier/',include('courier.urls'))
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
