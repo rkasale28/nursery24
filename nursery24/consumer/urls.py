72a0f7f84a61cf674a07b704034d2ff163076476
rkasale28 <rohitkasale28@gmail.com>
2020-03-20 15:36:39 +0530

    Login And Signup Done for Customer

--- nursery24/consumer/urls.py
+++ nursery24/consumer/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+    path('signup',views.signup,name="signup"),
+    path('signup_submit',views.signup_submit,name="signup_submit"),
+    path('login',views.login,name="login"),
+    path('login_submit',views.login_submit,name="login_submit"),
+    path('logout',views.logout,name="logout"),
+    path('myprofile',views.myprofile,name="myprofile"),
     path('home',views.home,name="home"),
     path('plants',views.plants,name="plants"),
     path('seeds',views.seeds,name="seeds"),
     path('soilandfertilizers',views.soil,name="soil"),
     path('decor',views.decor,name="decor"),
     path('accessories',views.accessories,name="accessories"),
-    path('sample',views.sample,name="Sample")
 ]
--- nursery24/consumer/views.py
+++ nursery24/consumer/views.py
@@ -1,6 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import IntegrityError
+from django.contrib.auth.models import User,auth
+from .models import Consumer
 
 # Create your views here.
+def signup(request):
+    return render(request,'csignup.html')
+
+def signup_submit(request):
+    try:
+        if (request.method=='POST'):
+            fname=request.POST["fname"]
+            lname=request.POST["lname"]
+            email=request.POST["mail"]
+            uname=request.POST["uname"]
+            pwd=request.POST["pwd"]
+            phone=request.POST["phone"]
+            user=User.objects.create_user(first_name=fname,last_name=lname,email=email,username=uname,password=pwd)    
+            consumer=Consumer(user=user,phone_number=phone,user_type='Consumer')
+            consumer.save()
+            return HttpResponse ("Info added to database<br>"+fname+"<br>"+lname+"<br>"+email+"<br>"+uname+"<br>"+pwd+"<br>"+phone)
+    except IntegrityError as e:
+        return HttpResponse ('Username already exists')
+
+def login(request):
+    return render(request,'clogin.html')
+
+def login_submit(request):
+    if request.method=='POST':
+        uname=request.POST["uname"]
+        pwd=request.POST["pwd"]
+        user=auth.authenticate(username=uname,password=pwd)
+        if user is not None:
+            auth.login(request,user)
+            return render(request,'chome.html')
+        else:
+            return HttpResponse('Invalid Credentials')
+    else:
+        return render(request,'login')
+
+def logout(request):
+    auth.logout(request)
+    return render (request,"chome.html")
+
+def myprofile(request):
+    return HttpResponse('My Profile')
+    
 def home(request):
     return render(request,'chome.html')
 
