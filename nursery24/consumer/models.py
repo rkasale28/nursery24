b448e06e3419aa06ae6dda26b181c8fd5c909a4a
rkasale28 <rohitkasale28@gmail.com>
2020-04-20 08:49:22 +0530

    Changed Order and ProductInOrder Table

--- nursery24/consumer/models.py
+++ nursery24/consumer/models.py
@@ -13,25 +13,27 @@ class Address(models.Model):
     consumer=models.ForeignKey(Consumer,on_delete=models.CASCADE)
 
 class Order(models.Model):
+    total_price=models.IntegerField()
+    
+class ProductInOrder(models.Model):
+    product=models.OneToOneField(Product,on_delete=models.CASCADE)
+    provider=models.OneToOneField(Provider,on_delete=models.CASCADE)
+    consumer=models.ForeignKey(Consumer,on_delete=models.CASCADE,null=True)
+    order=models.ForeignKey(Order,on_delete=models.CASCADE,null=True)
+    quantity=models.IntegerField(null=True)
+    total_price=models.IntegerField(null=True)
     ORDER_STATUS_CHOICES=[
         ('D','Delivered'),
         ('S','Shipped'),
-        ('P','Placed')
+        ('P','Placed'),
+        ('C','Cancelled')
     ]
     status=models.CharField(max_length=1,choices=ORDER_STATUS_CHOICES,default='P')
-    total_price=models.IntegerField()
-    date_last_tracked=models.DateField()
-    date_delivered=models.DateField()
-    consumer=models.ForeignKey(Consumer,on_delete=models.CASCADE)
-    last_tracked_by=models.OneToOneField(Courier,on_delete=models.CASCADE)
-
-class ProductInOrder(models.Model):
-    product=models.OneToOneField(Product,on_delete=models.CASCADE)
-    order=models.ForeignKey(Order,on_delete=models.CASCADE)
-    provider=models.OneToOneField(Provider,on_delete=models.CASCADE)
-    quantity=models.IntegerField()
-    total_price=models.IntegerField()
-
+    expected_delivery_date=models.DateField(null=True)
+    date_delivered=models.DateField(null=True)
+    date_last_tracked=models.DateField(null=True)
+    last_tracked_by=models.OneToOneField(Courier,on_delete=models.CASCADE,null=True)
+    
 class Review(models.Model):
     comment=models.CharField(max_length=250,null=True)
     rating=models.FloatField()
