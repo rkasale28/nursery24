c3a0ec977a5ea3d3e34bb1dbbbcf6ed8aabc2501
rkasale28 <rohitkasale28@gmail.com>
2020-04-21 16:22:20 +0530

    Designed Search Page

--- nursery24/nursery24/settings.py
+++ nursery24/nursery24/settings.py
@@ -69,6 +69,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+            'custom_tags':'template_tags.custom_tags'
+            }
         },
     },
 ]
