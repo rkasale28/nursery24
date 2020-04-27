d9aa468cfcdcfffe82b32956a5bab2d795776073
Ameya Keskar <ameyakeskar1999@gmail.com>
2020-04-27 19:21:57 +0530

    small changes ...

--- nursery24/static/js/cartTwo.js
+++ nursery24/static/js/cartTwo.js
@@ -27,7 +27,10 @@ integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9If
         <h6 class="card-subtitle text-muted">Rs. <span id='price'></span></h6>
     </div>
     <div class="btn-toolbar ml-1 mb-2" role="toolbar" aria-label="Toolbar with button groups">
-        
+         
+    <div class="btn-group mr-2" role="group" aria-label="Second group">
+    <button type="button" id="compare" class="btn btn-primary">Compare Price</button>
+</div>
         
         <div class="btn-group mr-2" role="group" aria-label="First group">
             <button type="button" class="btn btn-success" id = "inc">+</button>
@@ -52,7 +55,9 @@ class CartTwo extends HTMLElement{
         // })
         
         var btn=this.shadowRoot.querySelector('#compare')
-
+        btn.onclick= (event) =>{
+            location.href='/consumer/compareprices?id='+this.getAttribute('id')
+        }
         
         //this.innerHTML=`${this.getAttribute('name')}`
         let decodedCookie = decodeURIComponent(document.cookie).split(';');
