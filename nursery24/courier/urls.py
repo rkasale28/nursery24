1b6ff2b5174e6c14adbefccc095d5e7516ed96e9
rkasale28 <rohitkasale28@gmail.com>
2020-04-28 15:31:45 +0530

    Designing Courier Pages

--- nursery24/courier/urls.py
+++ nursery24/courier/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns=[
     path('home',views.home,name="home"),
+    path('signup',views.signup,name="signup"),
+    path('signup_submit',views.signup_submit,name="signup_submit"),
+    path('login',views.login,name="login"),
+    path('login_submit',views.login_submit,name="login_submit"),
+    path('logout',views.logout,name="logout"),
 ]
--- nursery24/courier/views.py
+++ nursery24/courier/views.py
@@ -1,5 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseRedirect
+from django.db import IntegrityError
+from django.contrib.auth.models import User,auth
+from .models import Courier,Address
 
 # Create your views here.
 def home(request):
     return render(request,'cohome.html')
+
+def signup(request):
+    return render(request,'cosignup.html')
+
+def signup_submit(request):
+    try:
+        if (request.method=='POST'):
+            sname=request.POST["sname"]
+            email=request.POST["mail"]
+            uname=request.POST["uname"]
+            pwd=request.POST["pwd"]
+            phone=request.POST["phone"]
+            addr=request.POST["address"]
+            user=User.objects.create_user(email=email,username=uname,password=pwd)    
+            courier=Courier(user=user,phone_number=phone,service_name=sname)
+            courier.save()
+            address=Address(addr=addr,courier=courier)
+            address.save()
+            user=auth.authenticate(username=uname,password=pwd)
+            auth.login(request,user)
+            return redirect('../courier/home')
+    except IntegrityError as e:
+        return HttpResponse ('Username already exists')
+
+def login(request):
+    return render(request,'cologin.html')
+
+def login_submit(request):
+    if request.method=='POST':
+        uname=request.POST["uname"]
+        pwd=request.POST["pwd"]
+        user=auth.authenticate(username=uname,password=pwd)
+        if user is not None:
+            try:
+                courier=Courier.objects.get(user=user)
+            except ObjectDoesNotExist as d:
+                return HttpResponse('User does not exist')
+            else:
+                auth.login(request,user)
+                U = cookies.SimpleCookie()
+                U['username'] = user
+                
+                return redirect('../courier/home')
+        else:
+            return HttpResponse('Invalid Credentials')
+    else:
+        return render(request,'login')
+
+def logout(request):
+    auth.logout(request)
+    print("Reached here")
+    return redirect('../courier/home')
