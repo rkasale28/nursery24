0a1debb162969b14e93fbaf7ad7155f7af08e67a
rkasale28 <rohitkasale28@gmail.com>
2020-04-28 18:13:52 +0530

    Added Default Images in Provider and Courier

--- nursery24/courier/models.py
+++ nursery24/courier/models.py
@@ -6,7 +6,7 @@ class Courier(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     phone_number=models.CharField(max_length=10,blank=False)
     service_name=models.CharField(max_length=100,null=True)
-    profile_pic=models.ImageField(upload_to='dps/',default='dps/profile.png')
+    profile_pic=models.ImageField(upload_to='dps/',default='dps/courier.jpg')
 
 class Address(models.Model):
     addr=models.TextField(max_length=100,null=True)
