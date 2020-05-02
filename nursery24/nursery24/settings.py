76aba42b0becfb5fb47e95087297d81e6f9545e3
rkasale28 <rohitkasale28@gmail.com>
2020-05-02 09:40:43 +0530

    Minor Changes

--- nursery24/nursery24/settings.py
+++ nursery24/nursery24/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nursery24',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'postgres',
         'HOST':'localhost'
     }
 }
