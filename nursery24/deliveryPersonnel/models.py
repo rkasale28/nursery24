4aa5388a40dd195cc6dd0311bb8864bc5aa7eb5b
rkasale28 <rohitkasale28@gmail.com>
2020-05-03 20:24:02 +0530

    Added Available Field in DeliveryPersonnel instead of offday

--- nursery24/deliveryPersonnel/models.py
+++ nursery24/deliveryPersonnel/models.py
@@ -4,20 +4,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class DeliveryPersonnel(models.Model):
-    OFFDAY_CHOICES=[
-        ('Sun','Sunday'),
-        ('Mon','Monday'),
-        ('Tue','Tuesday'),
-        ('Wed','Wednesday'),
-        ('Thu','Thursday'),
-        ('Fri','Friday'),
-        ('Sat','Saturday')
-    ]
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     phone_number=models.CharField(max_length=10,blank=False)
     profile_pic=models.ImageField(upload_to='dps/',default='dps/profile.png')
     assigned=models.BooleanField(default=False)
-    offday=models.CharField(max_length=3,choices=OFFDAY_CHOICES,default='Sun')
+    available=models.BooleanField(default=True)
     existing_location_addr=models.TextField(max_length=100,null=True)
     existing_location_point = gismodel.PointField(null=True)
     
