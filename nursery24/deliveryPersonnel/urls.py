0992ee2f61b1078a87e35d921adfe6a04b78fe95
rkasale28 <rohitkasale28@gmail.com>
2020-05-04 12:35:09 +0530

    Added logout for Delivery Personnel

--- nursery24/deliveryPersonnel/urls.py
+++ nursery24/deliveryPersonnel/urls.py
@@ -5,8 +5,8 @@ urlpatterns=[
     path('home',views.home,name="home"),
     path('login',views.login,name="login"),
     path('login_submit',views.login_submit,name="login_submit"),
-    # path('logout',views.logout,name="logout"),
-    # path('myprofile',views.myprofile,name="myprofile"),
+    path('logout',views.logout,name="logout"),
+    path('myprofile',views.myprofile,name="myprofile"),
     # path('edit',views.edit,name="edit"),
     # path('editsubmit',views.editsubmit,name="editsubmit"),
 ]
